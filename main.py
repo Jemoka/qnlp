2c99e7d776840d95c8868387d58f742dad1de72a
Houjun Liu <houjun@jemoka.com>
2023-03-19 12:09:20 -0700

    more changes

--- main.py
+++ main.py
@@ -1,17 +1,40 @@
 import numpy as np
 
-
-def densityp(Pl, Pr):
+def densityp(Pl:np.matrix, Pr:np.matrix):
     return np.trace(Pl @ Pr) == 1
 
-# def semidefinite()
+def phi(Pl:np.matrix, M:np.matrix, Pr:np.matrix):
+    return np.trace(Pl@M@Pr@M.getH())
 
-np.linalg.eigvals(np.array([[1+2j, 2+3j],
-                            [0, 1]]))
+def semidefinitify(m:np.matrix):
+    """take m and appling to its conjugate transpose, hence returning a semidefinte matrix
 
-densityp(np.array([[1+2j, 3+3j],
-                   [0, 1]]),
-         np.array([[1-5j, 2+0j],
-                   [1, 1]]))
+    Conditions for semidefinity: z* M z is real positive for any nonzero complex vector z
+
+    Small proof for why this works:
+    z* M M* z = (M* z)* (M* z); and so the inner product between them will cause negative
+      by negative, imag to imag, making it real and positive
+
+    Question for ted: wouldn't this make all M real? Isn't that a problem?
+    """
+    return m @ m.getH()
+
+
+
+semidefinitify(np.matrix(np.random.rand(5,5) + 1j * np.random.rand(5,5)))
+
+np.linalg.eigvals(semidefinitify(np.matrix([[1+8j, 2-8j],
+                                            [0, 1]])))
+
+semidefinitify(np.matrix([[1+8j, 2-8j],
+                          [0, 1]]))
+
+densityp(np.matrix([[1+2j, 3+3j],
+                    [0, 1]]),
+         np.matrix([[1-5j, 2+0j],
+                    [1, 1]]))
+
+type(np.matrix([[1-5j, 2+0j],
+                [1, 1]]))
 
 
